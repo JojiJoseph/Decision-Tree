b8092d3d1d518cfe3dad3ca5c418eaa0fefdc96c
joji <jojinattar@gmail.com>
2022-10-01 19:28:39 +0530

    Update regression test

--- test_cart_sine.py
+++ test_cart_sine.py
@@ -12,9 +12,9 @@ X_data = np.linspace(0,1,10).reshape((-1,1))
 y_data = np.sin(2*np.pi*X_data).flatten()
 # X_train, X_test, y_train, y_test = train_test_split(X_data, y_data)
 
-cart_reg = CART(impurity_function="mse", max_depth=5)
+cart_reg = CART(impurity_function="mse", max_depth=3)
 
-clf = DecisionTreeRegressor(max_depth=5)
+clf = DecisionTreeRegressor(max_depth=3)
 clf.fit(X_data, y_data)
 
 cart_reg.fit(X_data, y_data)
@@ -34,12 +34,12 @@ y_test = np.sin(2*np.pi*X_test).flatten()
 y_pred2 = cart_reg.predict(X_test)
 
 plt.plot(X_test.flatten(), y_test)
-plt.plot(X_test.flatten(), y_pred1, label="max depth=5")
+plt.plot(X_test.flatten(), y_pred1, label="max depth=3")
 plt.plot(X_test.flatten(), y_pred2, label="max depth=10")
 plt.legend()
 plt.show(block=False)
 
-reg = DecisionTreeRegressor(max_depth=5)
+reg = DecisionTreeRegressor(max_depth=3)
 reg.fit(X_data, y_data)
 
 
@@ -53,10 +53,12 @@ reg.fit(X_data, y_data)
 y_pred2 = reg.predict(X_test)
 plt.figure()
 plt.plot(X_test.flatten(), y_test)
-plt.plot(X_test.flatten(), y_pred1, label="max depth=5")
+plt.plot(X_test.flatten(), y_pred1, label="max depth=3")
 plt.plot(X_test.flatten(), y_pred2, label="max depth=10")
 plt.legend()
 # plt.plot(X_test.flatten(), y_pred2, label="max depth=20")
 plt.show()
 
 print(export_text(reg))
+
+print(get_explanation(cart_reg))
